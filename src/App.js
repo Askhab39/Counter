3b0f062c5861c0c34a36737fbb5ef0e23dbb0c4e
Askhab39 <Abdrazakov393@gmail.com>
2023-02-15 17:06:07 +0300

    Счетчик с возможностью сохранения значения в localStorage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import React from "react"
+import { useEffect } from "react";
 import './index.scss';
-
 function App() {
+  let [count, setCount] = React.useState(Number(localStorage.getItem('counter')) || 0)
+  let onClickPlus = () => {
+    setCount(count + 1)
+  }
+  useEffect(()=>{
+    localStorage.setItem('counter', count)
+  }, [count])
+  let onClickMinus = () => {
+    if (count < 1) {
+      setCount(count = 0)
+    } else {
+      setCount(count - 1)
+    }
+  }
+  useEffect(()=>{
+    localStorage.setItem('counter', count)
+  }, [count])
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
 }
-
 export default App;
