d5a2ae4b05afdcf725a3fc08355cd42012ab39af
Askhab39 <Abdrazakov393@gmail.com>
2023-02-19 14:34:58 +0300

    добавлена кнопка сброса с функционалом

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { useEffect } from "react";
 import './index.scss';
 function App() {
   let [count, setCount] = React.useState(Number(localStorage.getItem('counter')) || 0)
+
   let onClickPlus = () => {
     setCount(count + 1)
   }
+
   useEffect(()=>{
     localStorage.setItem('counter', count)
   }, [count])
+
   let onClickMinus = () => {
     if (count < 1) {
       setCount(count = 0)
@@ -16,14 +19,21 @@ function App() {
       setCount(count - 1)
     }
   }
+
+  let onClickZero = () => {
+    setCount(count = 0)
+  }
+
   useEffect(()=>{
     localStorage.setItem('counter', count)
   }, [count])
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
         <h1>{count}</h1>
+        <button onClick={onClickZero} className="zero">Сброс</button>
         <button onClick={onClickMinus} className="minus">- Минус</button>
         <button onClick={onClickPlus} className="plus">Плюс +</button>
       </div>
